c9967b3d75d0d008404ed515c5aa0a3c9a5f5994
Stefan Wilkes <stefan.wilkes@gmail.com>
2021-08-11 21:11:07 +0200

    Added public documentation and unit test to client

---
+++ MagicCardTracker.ScryfallClient.Tests/ScryfallClientFactoryTests.cs
@@ -0,0 +1,68 @@
+using System.Net.Http;
+using NSubstitute;
+using Xunit;
+
+namespace MagicCardTracker.ScryfallClient.Tests
+{
+    public class ScryfallClientFactoryTests
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        private readonly HttpClient _knownHttpClient;
+
+        public ScryfallClientFactoryTests()
+        {
+            _httpClientFactory = Substitute.For<IHttpClientFactory>();
+        }
+
+        [Fact]
+        public void TestCardsShouldReturnValidClient()
+        {
+            var sut = new ScryfallClientFactory(_httpClientFactory);
+            var cardsClient = sut.Cards;
+
+            Assert.IsType<CardsClient>(cardsClient);
+            Assert.NotNull(cardsClient);
+        }
+
+        [Fact]
+        public void TestSetsShouldReturnValidClient()
+        {
+            var sut = new ScryfallClientFactory(_httpClientFactory);
+            var setsClient = sut.Sets;
+
+            Assert.IsType<SetsClient>(setsClient);
+            Assert.NotNull(setsClient);
+        }
+
+        [Fact]
+        public void TestCatalogShouldReturnValidClient()
+        {
+            var sut = new ScryfallClientFactory(_httpClientFactory);
+            var catalogClient = sut.Catalog;
+
+            Assert.IsType<CatalogClient>(catalogClient);
+            Assert.NotNull(catalogClient);
+        }
+
+        [Fact]
+        public void TestSymbologyShouldReturnValidClient()
+        {
+            var sut = new ScryfallClientFactory(_httpClientFactory);
+            var symbologyClient = sut.Symbology;
+
+            Assert.IsType<SymbologyClient>(symbologyClient);
+            Assert.NotNull(symbologyClient);
+        }
+
+        [Fact]
+        public void TestRulingsShouldReturnValidClient()
+        {
+            var sut = new ScryfallClientFactory(_httpClientFactory);
+            var rulingsClient = sut.Rulings;
+
+            Assert.IsType<RulingsClient>(rulingsClient);
+            Assert.NotNull(rulingsClient);
+        }
+    }
+}
--- MagicCardTracker.ScryfallClient/ScryfallClientFactory.cs
+++ MagicCardTracker.ScryfallClient/ScryfallClientFactory.cs
@@ -2,23 +2,36 @@ using System.Net.Http;
 
 namespace MagicCardTracker.ScryfallClient
 {
+    /// <summary>
+    ///     Implementation of a IScryfallClientFactory that will construct
+    ///     proper generated client services using a IHttpClientFactory.
+    /// </summary>
     public class ScryfallClientFactory : IScryfallClientFactory
     {
         private readonly IHttpClientFactory _clientFactory;
 
+        /// <summary>
+        ///     Creates a new ScryfallClientFactory
+        /// </summary>
+        /// <param name="clientFactory"> An instance of a http client factory </param>
         public ScryfallClientFactory(IHttpClientFactory clientFactory)
         {
             _clientFactory = clientFactory;
         }
         
+        /// <inheritdoc />
         public ICardsClient Cards => new CardsClient(_clientFactory.CreateClient());
 
+        /// <inheritdoc />
         public ISetsClient Sets => new SetsClient(_clientFactory.CreateClient());
 
+        /// <inheritdoc />
         public ISymbologyClient Symbology => new SymbologyClient(_clientFactory.CreateClient());
 
+        /// <inheritdoc />
         public ICatalogClient Catalog => new CatalogClient(_clientFactory.CreateClient());
 
+        /// <inheritdoc />
         public IRulingsClient Rulings => new RulingsClient(_clientFactory.CreateClient());
     }
 }
