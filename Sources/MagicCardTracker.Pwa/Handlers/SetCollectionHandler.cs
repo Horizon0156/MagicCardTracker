4abed5f1e41e7a31ecec1843d130a3691bbef98d
Stefan W <stefan.wilkes@gmail.com>
2021-08-18 18:35:41 +0200

    Added message for set updates

--- Sources/MagicCardTracker.Pwa/Handlers/SetCollectionHandler.cs
+++ Sources/MagicCardTracker.Pwa/Handlers/SetCollectionHandler.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using AutoMapper;
 using MagicCardTracker.Pwa.Cache;
 using MagicCardTracker.Pwa.Commands;
+using MagicCardTracker.Pwa.Notifications;
 using MagicCardTracker.Pwa.Queries;
 using MagicCardTracker.ScryfallClient;
 using MagicCardTracker.Storage;
@@ -23,17 +24,20 @@ namespace MagicCardTracker.Pwa.Handlers
         private readonly IScryfallClientFactory _scryfallClientFactory;
         private readonly IObjectCache _cache;
         private readonly IMapper _mapper;
+        private readonly INotificationService _notificationService;
 
         public SetCollectionHandler(
             ICardLibrary cardLibrary,
             IScryfallClientFactory scryfallClientFactory,
             IObjectCache cache,
-            IMapper mapper)
+            IMapper mapper,
+            INotificationService notificationService)
         {
             _cardLibrary = cardLibrary;
             _scryfallClientFactory = scryfallClientFactory;
             _cache = cache;
             _mapper = mapper;
+            _notificationService = notificationService;
         }
 
         public async Task<IEnumerable<Set>> Handle(
@@ -58,7 +62,8 @@ namespace MagicCardTracker.Pwa.Handlers
 
         public async Task<Unit> Handle(UpdateSets request, CancellationToken cancellationToken)
         {
-            await GetCommonSetsAsync(cancellationToken);
+            var sets = await GetCommonSetsAsync(cancellationToken);
+            _notificationService.SendNotification(new Notification($"Updated information for {sets.Count} sets"));
 
             return Unit.Value;
         }
